95dec0443bc1acf16dfc03c68460426e3e959d86
FernandoDiasH <fernandohiroshi10@hotmail.com>
2023-02-13 17:45:09 -0300

    Alterando as configuracoes do git

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import { resolver } from './utils/errors/ErrorResolver';
 const routes = Router();
 
 routes.get('/', resolver(async (req:Request, res:Response) => {
+    console.log('Log teste');
     return res.send('Controle financeiro');
 }));
 
