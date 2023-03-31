c5833d04ecb127461f7cb4710270a3b94037a571
FernandoDiasH <fernandohiroshi10@hotmail.com>
2023-03-31 17:58:17 -0300

    initial commit

---
+++ src/infra/http/controllers/analyze.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from "@nestjs/common";
+
+
+@Controller()
+export class AnalyzeController {
+
+
+    @Get()
+    async analyze(){
+
+        //pegar todas saidas e somar
+        //pegar todas entradas e somar
+
+        //pegar entrada do mes e soma 
+        // pegar saida do mes e somar
+
+        //pegar todos os creditos e somar
+        //pegar os creditos do mes 
+
+
+
+    }
+}
